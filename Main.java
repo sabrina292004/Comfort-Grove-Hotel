3c3fcf1def8b9cad0a76cb59e3bc51481a98a930
sabrina292004 <154123154+sabrina292004@users.noreply.github.com>
2024-03-03 21:12:51 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package JavaOOP;
+public class Main {
+    // A main method that creates an instance of the system and displays the home page
+    public static void main(String[] args) {
+        HotelManagementSystem hotel = new HotelManagementSystem();
+        hotel.displayHomePage();
+        }
+    }
