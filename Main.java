2fa719ff76de4c40dd478c5ce4762bfe66acaf3b
sabrina292004 <154123154+sabrina292004@users.noreply.github.com>
2024-03-06 21:09:43 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package JavaOOP;
-public class Main {
-    // A main method that creates an instance of the system and displays the home page
-    public static void main(String[] args) {
-        HotelManagementSystem hotel = new HotelManagementSystem();
-        hotel.displayHomePage();
-        }
-    }
